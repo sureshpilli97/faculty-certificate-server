708a5f3530bddbb3bc9f045a6a5764cf04fbd70b
suresh pilli <sureshpilli465@gmail.com>
2024-06-23 15:27:40 +0530

    feat: first deploy commit

---
+++ server.js
@@ -0,0 +1,142 @@
+const mysql = require('mysql2/promise');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+async function connectToDatabase() {
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+
+    });
+    return connection;
+  } catch (err) {
+    console.error('Failed to connect to the database:', err);
+    throw err;
+  }
+}
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Faculty Certification SVEC Backend Server!');
+});
+
+app.post('/insert', async (req, res) => {
+  const { faculty_id, name, branch, email } = req.body;
+  const query = 'INSERT INTO faculty (faculty_id, name, branch, email) VALUES (?, ?, ?, ?)';
+
+  try {
+    const db = await connectToDatabase();
+    const [result] = await db.execute(query, [faculty_id, name, branch, email]);
+    await db.end();
+    res.status(201).send({ message: 'Faculty details inserted successfully!' });
+  } catch (err) {
+    console.error('Failed to insert faculty details:', err);
+    res.status(500).send({ error: 'Failed to insert faculty details.' });
+  }
+});
+
+app.post('/upload', async (req, res) => {
+  const { faculty_id, certificate_name, type_of_certification, certificate_url } = req.body;
+  const query = 'INSERT INTO faculty_certificates (faculty_id, certificate_name, type_of_certification, certificate_url) VALUES (?, ?, ?, ?)';
+
+  try {
+    const db = await connectToDatabase();
+    const [result] = await db.execute(query, [faculty_id, certificate_name, type_of_certification, certificate_url]);
+    await db.end();
+    res.status(201).send({ message: 'Certificate details inserted successfully!' });
+  } catch (err) {
+    console.error('Failed to insert certificate details:', err);
+    res.status(500).send({ error: 'Failed to insert certificate details.' });
+  }
+});
+
+app.get('/retrieve_faculty', async (req, res) => {
+  const { conditions, columns } = req.query;
+  let query = 'SELECT ';
+
+  if (columns) {
+    query += `${columns}`;
+  } else {
+    query += '*';
+  }
+
+  query += ' FROM faculty';
+
+  if (conditions) {
+    query += ` WHERE ${conditions}`;
+  }
+
+  try {
+    const db = await connectToDatabase();
+    const [result] = await db.execute(query);
+    await db.end();
+    res.status(200).send(result);
+  } catch (err) {
+    console.error('Failed to retrieve faculty data:', err);
+    res.status(500).send({ error: 'Failed to retrieve faculty data.' });
+  }
+});
+
+app.get('/retrieve_certificates', async (req, res) => {
+  const { conditions, columns } = req.query;
+  let query = 'SELECT ';
+
+  if (columns) {
+    query += `${columns}`;
+  } else {
+    query += '*';
+  }
+
+  query += ' FROM faculty_certificates';
+
+  if (conditions) {
+    query += ` WHERE ${conditions}`;
+  }
+
+  try {
+    const db = await connectToDatabase();
+    const [result] = await db.execute(query);
+    await db.end();
+    res.status(200).send(result);
+  } catch (err) {
+    console.error('Failed to retrieve certificate data:', err);
+    res.status(500).send({ error: 'Failed to retrieve certificate data.' });
+  }
+});
+
+app.get('/count', async (req, res) => {
+  const { conditions } = req.query;
+  let query = 'SELECT COUNT(*) AS count FROM faculty_certificates';
+
+  if (conditions) {
+    query += ` WHERE ${conditions}`;
+  }
+
+  try {
+    const db = await connectToDatabase();
+    const [result] = await db.execute(query);
+    await db.end();
+    res.status(200).send({ count: result[0].count });
+  } catch (err) {
+    console.error('Failed to count certificate records:', err);
+    res.status(500).send({ error: 'Failed to count certificate records.' });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('Server is running...');
+});
